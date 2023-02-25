b155992306336b0cee2ccceeddf7a9762053253f
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2023-02-25 22:26:50 +0300

    Add configuration for Central publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,35 @@
 plugins {
     id("build-logic.root-build")
+    id("com.github.vlsi.stage-vote-release") version "1.86"
 }
 
-val buildVersion = "${findProperty("version")}"
+val buildVersion = "${findProperty("version")}${releaseParams.snapshotSuffix}"
 
 println("Building Argument Expression $buildVersion")
+
+val isReleaseVersion = rootProject.releaseParams.release.get()
 
 allprojects {
     group = "io.github.vlsi.kae"
     version = project.findProperty("version") as? String ?: rootProject.version
+}
+
+releaseParams {
+    tlp.set("kotlin-argument-expression")
+    organizationName.set("vlsi")
+    componentName.set("kotlin-argument-expression")
+    prefixForProperties.set("s01")
+    svnDistEnabled.set(false)
+    sitePreviewEnabled.set(false)
+    nexus {
+        prodUrl.set(uri("https://s01.oss.sonatype.org"))
+    }
+    voteText.set {
+        """
+        ${it.componentName} v${it.version}-rc${it.rc} is ready for preview.
+
+        Git SHA: ${it.gitSha}
+        Staging repository: ${it.nexusRepositoryUri}
+        """.trimIndent()
+    }
 }
