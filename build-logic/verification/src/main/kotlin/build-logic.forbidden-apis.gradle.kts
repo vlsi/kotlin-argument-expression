b866c6d69ff23ad6c798e817e34c9fd7c0a700ad
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2023-02-25 20:33:57 +0300

    Add failOnMissingClasses = false to forbiddenApis

--- build-logic/verification/src/main/kotlin/build-logic.forbidden-apis.gradle.kts
+++ build-logic/verification/src/main/kotlin/build-logic.forbidden-apis.gradle.kts
@@ -7,6 +7,10 @@ plugins {
 
 forbiddenApis {
     failOnUnsupportedJava = false
+    // ForbiddenApiException: Check for forbidden API calls failed while scanning class 'Dev_sigstore_sign_base_gradle'
+    // (dev.sigstore.sign-base.gradle.kts): java.lang.ClassNotFoundException: kotlin.script.experimental.jvm.RunnerKt
+    // (while looking up details about referenced class 'kotlin.script.experimental.jvm.RunnerKt')
+    failOnMissingClasses = false
     signaturesFiles = files("$rootDir/config/forbidden-apis/forbidden-apis.txt")
     bundledSignatures.addAll(
         listOf(
