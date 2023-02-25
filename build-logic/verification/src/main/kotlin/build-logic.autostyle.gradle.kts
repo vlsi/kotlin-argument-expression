60600d0d37e066258d0c29ae473f2785b5c631b2
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2023-02-25 20:33:25 +0300

    remove Java configuration from Autostyle (there's no Java in project)

--- build-logic/verification/src/main/kotlin/build-logic.autostyle.gradle.kts
+++ build-logic/verification/src/main/kotlin/build-logic.autostyle.gradle.kts
@@ -22,21 +22,5 @@ autostyle {
 
 plugins.withId("java") {
     autostyle {
-        java {
-            // targetExclude("**/test/java/*.java")
-            // TODO: implement license check (with copyright year)
-            // licenseHeaderFile(licenseHeaderFile)
-            importOrder(
-                "static ",
-                "org.postgresql.",
-                "",
-                "java.",
-                "javax."
-            )
-            removeUnusedImports()
-            trimTrailingWhitespace()
-            indentWithSpaces(4)
-            endWithNewline()
-        }
     }
 }
