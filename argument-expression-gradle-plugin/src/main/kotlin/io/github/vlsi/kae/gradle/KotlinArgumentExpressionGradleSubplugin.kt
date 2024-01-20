6f98ec3df370b39da4e8b01f4f795eef6db58b76
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2024-01-20 21:57:18 +0300

    Bump versions of the included argument-expression-plugin

--- argument-expression-gradle-plugin/src/main/kotlin/io/github/vlsi/kae/gradle/KotlinArgumentExpressionGradleSubplugin.kt
+++ argument-expression-gradle-plugin/src/main/kotlin/io/github/vlsi/kae/gradle/KotlinArgumentExpressionGradleSubplugin.kt
@@ -35,7 +35,7 @@ class KotlinArgumentExpressionGradleSubplugin : KotlinCompilerPluginSupportPlugi
         SubpluginArtifact(
             groupId = "io.github.vlsi.kae",
             artifactId = "argument-expression-plugin",
-            version = "1.0.3"
+            version = "1.0.6"
         )
 
     override fun isApplicable(kotlinCompilation: KotlinCompilation<*>): Boolean =
